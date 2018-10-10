fec2f4395633b0514404dad7474ee8402c16578c
Lukay <861234327@qq.com>
2018-10-10 10:59:23 +0800

    Initial Commit

---
+++ Server/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Server
+//
+//  Created by LK on 2018/10/10.
+//  Copyright © 2018 LK. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
